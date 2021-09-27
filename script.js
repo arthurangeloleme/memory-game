9caaded4d8c100a8c62f0bb2b6ef100588e58864
Arthur Angelo Leme <arthur.angelo@outlook.com>
2021-09-27 14:45:58 -0400

    Criado as funcionalidades das cartas

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card'); /* Chama a classe card*/
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard(){
-    this.classList.toggle('flip');
+    if(lockBoard) return;
+    if(this === firstCard) return;
+    this.classList.add('flip'); /*(função toggle tira e remove a classe) Cria a classe flip na div card*/ 
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+    hasFlippedCard = false;
+    checkFormMath();
 }
 
+function checkFormMath() {
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards();
+        return;
+    }
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500)
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+
 cards.forEach((card) => {
-    card.addEventListener('click', flipCard)
+    card.addEventListener('click', flipCard) /*Adiciona a classe flip ao clicar no card*/
 });
+
